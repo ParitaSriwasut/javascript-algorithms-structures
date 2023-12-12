9b0322e807539fe1019ecfd43939962084486cc1
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-13 04:31:26 +0700

    update lab01

--- big-o-notation/big-o-lab.js
+++ big-o-notation/big-o-lab.js
@@ -1,8 +1,8 @@
 //for loop is the worst case of bigO its executed many times depends on the input.
-//here in this code is 4+2 (n + 2) In this case, with n = 4, the loop will execute 4 times, resulting in a time complexity of O(4), which simplifies to O(n) in big-O notation.
-//for loop is going to iterates over the entire input and hence the time complexity.
-//time complexity. O(n) - Linear
+//for loop is going to iterates over the entire input and hence the time complexity. nested loop is took Time complexity O(n2)- Quadratic
 
+//here in this code is 4+2 (n + 2) In this case, with n = 4, the loop will execute 4 times, resulting in a time complexity of O(4), which simplifies to O(n) in big-O notation.
+//time complexity. O(n) - Linear
 // const summation = (n) =>
 // {
 //   let sum = 0; //execute 1
@@ -18,3 +18,8 @@ const summation = (n) => {
 };
 
 console.log(summation(4)); //10
+
+//Space complexity
+//O(1)-Constant
+//O(n)-Linear
+//O(logn)-Logarithmic
